4dfa39baa90f6eb88ffbdc1dd2b880c6972da803
Adam Tygart <adam.tygart@gmail.com>
2011-04-08 15:39:03 -0500

    Re-Adding functions for password account and session

--- pam_sge.c
+++ pam_sge.c
@@ -37,6 +37,11 @@
  * ld -x --shared -o pam_sge.so pam_sge.o
  */
 
+#define PAM_SM_AUTH
+#define PAM_SM_ACCOUNT
+#define PAM_SM_PASSWORD
+#define PAM_SM_SESSION
+
 #include <security/pam_modules.h>
 #include <sys/param.h>
 
@@ -194,6 +199,46 @@ pam_sm_setcred(pam_handle_t *pamh, int flags,
 {
     return (PAM_SUCCESS);
 }
+
+PAM_EXTERN int
+pam_sm_acct_mgmt (pam_handle_t *pamh, int flags,
+    int argc, const char **argv)
+{
+    return pam_sm_authenticate(pamh, flags, argc, argv);
+}
+
+PAM_EXTERN int
+pam_sm_open_session (pam_handle_t *pamh, int flags,
+    int argc, const char **argv)
+{
+    return pam_sm_authenticate(pamh, flags, argc, argv);
+}
+
+PAM_EXTERN int
+pam_sm_close_session (pam_handle_t *pamh, int flags,
+    int argc, const char **argv)
+{
+    return pam_sm_authenticate(pamh, flags, argc, argv);
+}
+
+PAM_EXTERN int
+pam_sm_chauthtok (pam_handle_t *pamh, int flags,
+    int argc, const char **argv)
+{
+    return pam_sm_authenticate(pamh, flags, argc, argv);
+}
+
+#ifdef PAM_STATIC
+struct pam_module _pam_sge_modstruct = {
+    "pam_sge",
+    pam_sm_authenticate,
+    pam_sm_setcred,
+    pam_sm_acct_mgmt,
+    pam_sm_open_session,
+    pam_sm_close_session,
+    pam_sm_chauthtok,
+};
+#endif
 
 #ifdef PAM_MODULE_ENTRY
 PAM_MODULE_ENTRY("pam_sge");
